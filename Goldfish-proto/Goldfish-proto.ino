513a08925ca64ee943a6df095435c113a01371a1
Nicole Martinez <nmartinez@argusinvent.com>
2017-07-06 00:21:59 -0500

    cleaned code up a bit

--- Goldfish-proto/Goldfish-proto.ino
+++ Goldfish-proto/Goldfish-proto.ino
@@ -4,18 +4,14 @@
 
 #define NUMPIXELS 60 // Number of LEDs in strip
 
-// LED data and clock pins:
-#define DATAPIN    11
-#define CLOCKPIN   13
-
 Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BGR);
 
-#define RPM 240
-//#define INTERVALS 60
+#define RPM 250 //eventually we won't need this.
 #define IMAGEWIDTH 8
 #define IMAGEHEIGHT 13
-//#define HEIGHT 8
+
 #define OFFSET 0
+#define RESOLUTION 3 //degrees per column in image array
 
 uint32_t colors[3] = {
   0x000000, //black
@@ -23,8 +19,8 @@ uint32_t colors[3] = {
   0x000044  //blue
 };
 
-//uint32_t orange = 0xff5000; //Starting color
-//uint32_t blue = 0x0000ff;
+//orange = 0xff5000; //Starting color
+//blue = 0x0000ff;
 // red = 0xFF0000;
 // green = 0x00ff00;
 // blue = 0x0000ff;
@@ -50,8 +46,8 @@ int image[IMAGEWIDTH][IMAGEHEIGHT] = {
 };
 
 unsigned long rps = RPM / 60;
-unsigned long microPerRotation = 1000000 / rps; //microseconds per rotation
-unsigned int loopInterval = microPerRotation / 120; //Time to travel 3 degrees in microseconds
+unsigned long microPerRotation = 1000000 / rps; //microseconds per rotation, this will eventually be determined by a hall effect sensor or something
+unsigned int loopInterval = microPerRotation / 360; //Time to travel 1 degree in microseconds
 
 //TODO: loopInterval should represent 1 degree for precision and then we can do some math to divide the current interval by 3 ro something
 
@@ -59,8 +55,7 @@ unsigned long startTime = 0;
 
 void setup() {
 
-  Serial.begin(9600);
-  //Serial.print(loopInterval);
+  //Serial.begin(9600);
  
   strip.begin(); // Initialize pins for output
   strip.clear();
@@ -70,41 +65,31 @@ void setup() {
 //int currentInterval = 0;
 void loop() {
 
-  int curInterval = currentInterval();
-  Serial.println(curInterval);
+  int curInterval = currentInterval() / RESOLUTION;
+  //Serial.println(curInterval);
 
-  if(curInterval <= IMAGEWIDTH && curInterval >= 0) {
-
-    bool resetPixels = curInterval == IMAGEWIDTH;
-    //int column[CIRCLEWIDTH] = image[curInterval];
+  if(curInterval < IMAGEWIDTH && curInterval >= 0) {
 
     for(int i = 0; i < IMAGEHEIGHT; i++) {
 
-      int pixelVal = resetPixels ? 0 : image[curInterval][i];
+      int pixelVal = image[curInterval][i];
       
       uint32_t pixelColor = colors[pixelVal];
-      //Serial.println(pixelColor);
-      //Serial.println();
+      
       strip.setPixelColor(i + OFFSET, pixelColor);
     }
+    
   } else {
     strip.clear();
   }
 
   strip.show();
-
-//  if(++currentInterval == INTERVALS) {
-//    currentInterval = 0;
-//  }
-//  
-//  delay(loopInterval);                        // Pause 20 milliseconds (~50 FPS)
 }
 
 //TODO: deal with rollover
 int currentInterval() {
   unsigned long now = micros();
   long timeDiff = now - startTime;
-  //Serial.println((long) 1 / rps);
 
   if(timeDiff > microPerRotation) {
     startTime = now;
